990c9b477d45ff0d16a010a403ebf29e27dc4528
ALTAMIRANOale <105112602+ALTAMIRANOale@users.noreply.github.com>
2022-08-29 21:36:54 -0300

    Delete Proyecto-master/blog directory
    deleteeee

--- Proyecto-master/blog/apps/noticias/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Noticia, Categoria, Comentario
-
-admin.site.register(Noticia)
-admin.site.register(Categoria)
-admin.site.register(Comentario)
